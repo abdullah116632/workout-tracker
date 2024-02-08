5d6653cc9a08dd527aedf16df17cdf625616b173
Abdullah al rafi <abdullah116632@gmail.com>
2024-02-08 21:38:45 +0600

    api connected

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,12 +2,12 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <div>
+    <header>
       <div className="container">
         <Link to="/">
-            <h1>Workout Buddy</h1>
+          <h1>Workout Buddy</h1>
         </Link>
       </div>
-    </div>
+    </header>
   );
 }
